28c4e2e7bc6e2e9f3468c6fbd3f75502cb7bf0fa
Reza Alizadeh Majd <r.a.majd@gmail.com>
2024-02-18 01:09:23 +0330

    working on new design for the blog

---
+++ publish.el
@@ -0,0 +1,52 @@
+(add-to-list 'load-path "~/.emacs.d/straight/repos/weblorg")
+(add-to-list 'load-path "~/.emacs.d/straight/repos/emacs-htmlize")
+(add-to-list 'load-path "~/.emacs.d/straight/repos/templatel")
+
+(require 'weblorg)
+
+(require 'htmlize)
+(setq org-html-htmlize-output-type 'css)
+
+
+(weblorg-site
+ :template-vars '(("site_name" . "RAMajd's daily notes")
+                  ("site_owner" . "Reza Alizadeh Majd <r.a.majd@gmail.com>")
+                  ("site_description" . "Notes about challenges I face during my day to day work")))
+
+(weblorg-route
+ :name "posts"
+ :input-pattern "posts/*.org"
+ :template "post.html"
+ :output "output/posts/{{ slug }}.html"
+ :url "/posts/{{ slug }}.html")
+
+
+(weblorg-route
+ :name "index"
+ :input-pattern "posts/*.org"
+ :input-aggregate #'weblorg-input-aggregate-all-desc
+ :template "index.html"
+ :output "output/index.html"
+ :url "/")
+
+(weblorg-route
+ :name "archive"
+ :input-pattern "posts/*.org"
+ :input-aggregate #'weblorg-input-aggregate-all-desc
+ :template "archive.html"
+ :output "output/archive.html"
+ :url "/archive.html")
+
+(weblorg-route
+ :name "pages"
+ :input-pattern "pages/*.org"
+ :template "page.html"
+ :output "output/{{ slug }}.html"
+ :url "/{{ slug }}.html")
+
+(weblorg-copy-static
+ :output "output/static/{{ file }}"
+ :url "/static/{{ file }}")
+
+
+(weblorg-export)
