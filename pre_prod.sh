73b0f03dd148f44fb99a9dd2ba0987b0f0f4d04c
Reza Alizadeh Majd <r.a.majd@gmail.com>
2024-02-20 11:34:47 +0330

    fix template load issue

--- pre_prod.sh
+++ pre_prod.sh
@@ -1,14 +1,12 @@
 echo "running pre_prod.sh"
 
 cat <<EOF > site.el
-(setq
- site
- (weblorg-site
+(weblorg-site
   :base-url "https://ramajd.github.io"
   :template-vars
   '(("site_name" . "RAMajd's daily notes")
     ("site_owner" . "Reza Alizadeh Majd <r.a.majd@gmail.com>")
-    ("site_description" . "Notes about challenges I face during my day to day work"))))
+    ("site_description" . "Notes about challenges I face during my day to day work")))
 
 (provide 'site)
 EOF
